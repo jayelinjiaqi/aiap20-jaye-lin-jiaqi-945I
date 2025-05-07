de7ccc83eb9f44c4438d53652b19eba02af1ac84
jayelin <jiaqi.lin@mail.com>
2025-05-08 06:23:42 +0800

    test #2 run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,11 @@
-sudo docker run --rm jayelinjiaqi/my-sqlite-app:latest python ml_pipeline.py
+# Create a temporary container to copy out the database
+docker create --name temp-sqlite jayelinjiaqi/my-sqlite-app:latest
+
+# Copy bmarket.db from Docker image to runner filesystem
+docker cp temp-sqlite:/data/bmarket.db ./data/bmarket.db
+
+# Remove temporary container
+docker rm temp-sqlite
+
+# Run your ML pipeline script using the extracted database
+python ml_pipeline.py
