9e8c27fd4ae1f93d4dc0aa1efb944fdbc31dde5d
jayelin <jiaqi.lin@mail.com>
2025-05-11 20:54:50 +0800

    test model

--- src/train_model.py
+++ src/train_model.py
@@ -4,7 +4,6 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import train_test_split
-import pickle
 
 def train_models(db_path, table_name):
     # Load and preprocess data
@@ -13,22 +12,29 @@ def train_models(db_path, table_name):
 
     X = df.drop(columns='Subscription Status')
     y = df['Subscription Status']
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=42)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, stratify=y, random_state=42)
 
     models = {
-        'RandomForest': RandomForestClassifier(random_state=5, n_estimators=300, max_depth=20, min_samples_leaf=4),
-        'LogisticRegression': LogisticRegression(max_iter=1000),
-        'KNN': KNeighborsClassifier(n_neighbors=5)
+        'RandomForest': RandomForestClassifier(random_state=42, n_estimators=300, max_depth=30, max_features='sqrt', min_samples_leaf=1),
+        'LogisticRegression': LogisticRegression(class_weight='balanced', max_iter=700, C=10, solver='liblinear', penalty='l1'),
+        'KNN': KNeighborsClassifier(n_neighbors=20, weights='uniform', metric='manhattan')
     }
 
     trained_models = {}
 
     for name, model in models.items():
         model.fit(X_train, y_train)
-        trained_models[name] = (model, X_test, y_test)
+        if hasattr(model, "predict_proba"):
+            preds_proba = model.predict_proba(X_test)[:, 1]
+        else:
+            preds_proba = None
 
-        # save model to disk
-        with open(f'{name}_model.pkl', 'wb') as f:
-            pickle.dump(model, f)
+        trained_models[name] = {
+            'model': model,
+            'X_test': X_test,
+            'y_test': y_test,
+            'preds_proba': preds_proba
+        }
 
     return trained_models
+
