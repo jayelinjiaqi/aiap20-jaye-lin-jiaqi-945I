62abbdae3a288912382252c7cfc4ad4a0a68ccd2
J LIN <116197057+jayelinjiaqi@users.noreply.github.com>
2025-05-12 15:46:18 +0800

    echo task in run.sh

--- run.sh
+++ run.sh
@@ -1,25 +1,30 @@
 #!/bin/bash
 
 # Debug
+echo "=== Running pipeline ==="
 echo "Current directory: $(pwd)"
-ls -al
 
 # Ensure the local data directory exists
 mkdir -p ./data
 
 # Create temporary container from image
+echo "Creating Docker container..."
 docker create --name temp-sqlite jayelinjiaqi/my-sqlite-app:latest
 
 # Copy .db file from the Docker container to local data directory
+echo "Copying database from container..."
 docker cp temp-sqlite:/data/bmarket.db ./data/bmarket.db
 
 # Clean up temporary container
+echo "Cleaning up Docker container..."
 docker rm temp-sqlite
 
-# Run the ML pipeline
-#python src/main.py
-
+# Parameters to db_path and table name
 db_path='/home/runner/work/aiap20-jaye-lin-jiaqi-945I/aiap20-jaye-lin-jiaqi-945I/data/bmarket.db'
 table_name='bank_marketing'
 
+# Run train_model.py in src folder
+echo "Running model training..."
 python src/train_model.py --db_path "$db_path" --table_name "$table_name"
+
+echo "=== Pipeline completed successfully ==="
